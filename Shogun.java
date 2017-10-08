d9a78e467194a1f61545aaa3b7d55995982fc777
woodchuck <22991547+woodchuck33@users.noreply.github.com>
2017-10-08 12:25:51 -0700

    Done with Mini-Ninjas and Mini-Samurai

--- Shogun.java
+++ Shogun.java
@@ -149,7 +149,9 @@ public class Shogun
 			}
 		}
 		// Finding the possible moves for the human player
-		// All moves described from the player's POV
+		// All moves described from the computer's POV
+		// Player moves from board[0][] to board[7][]
+		// Plyer's left is board[][0] and right is board[][7]
 		else
 		{
 			for (int length = 0; length < BOARD_LENGTH; length++)
@@ -159,36 +161,41 @@ public class Shogun
 					// Mini-Samurai
 					if (board[length][width]==-1)
 					{
-						// If there is no piece in front of the Mini-Samurai, it's possible to move forward one space
-						// Piece must also not be all the way at the end of the board
-						if (length>0 && board[length+1][width]==0)
+						// If there is no piece in front (board[length+1][]) of the Mini-Samurai, it's possible to move forward one space
+						// Mini-Samurai must also not be in (board[BOARD_LENGTH-1][])
+						if (length<BOARD_LENGTH-1 && board[length+1][width]==0)
 							moves.append((char)(width+'A')).append(length+1).append((char)(width+'A')).append(length+2+" ");
-						// If there is an enemy piece to the left AND forward of the Mini-Samurai AND there is no piece directly to the left...
-						// Width must be greater than 0
+						// If there is an enemy piece to the left (board[][width-1]) AND forward (board[length+1][]) of the Mini-Samurai 
+						// AND there is no piece directly to the left (board[length][width-1])...
+						// Mini-Samurai must not be in board[][0]
 						if (width>0 && board[length][width-1]==0 && board[length+1][width-1]>0)
 							moves.append((char)(width+'A')).append(length+1).append((char)(width-1+'A')).append(length+1+" ");
-						// If there is an enemy piece to the right AND forward of the Mini-Samurai AND there is no piece directly to the right...
-						// Width must be less than BOARD_WIDTH-1
+						// If there is an enemy piece to the right AND forward (board[length+1][width+1]) of the Mini-Samurai 
+						// AND there is no piece directly to the right (board[length][width+1])...
+						// Mini-Samurai must not be in board[][BOARD_WIDTH-1]
 						if (width<BOARD_WIDTH-1 && board[length][width+1]==0 && board[length+1][width+1]>0)
 							moves.append((char)(width+'A')).append(length+1).append((char)(width+1+'A')).append(length+1+" ");
 					}
 					// Mini-Ninja
 					else if (board[length][width]==-2)
 					{
-						// If there is no piece diagonally forward to the right of Mini-Ninja...
-						// Piece must be one away from the right side of the board and not all the way at the end of the board
-						if (length>0 && width<BOARD_WIDTH-1 && board[length-1][width+1]==0)
-							moves.append((char)(width+'A')).append(length+1).append((char)(width+1+'A')).append(length+" ");
-						// If there is no piece diagonally forward to the left of Mini-Ninja...
-						// Piece must be one away from the left side of the board and not all the way to the end of the board
-						if (length>0 && width>0 && board[length-1][width-1]==0)
-							moves.append((char)(width+'A')).append(length+1).append((char)(width-1+'A')).append(length+" ");
-						// If there is an enemy piece to the right with no piece behind it, Mini-Ninja can move there to attack
-						// Mini-Ninja must be one away from right side of the board and must be one away from the end of the board
-						if (length<BOARD_LENGTH-1 && width<BOARD_WIDTH-1 && board[length][width+1]<0 && board[length+1][width+1]==0)
+						// If there is no piece diagonally forward to the right (board[length+1][width+1]) of Mini-Ninja...
+						// Mini-Ninja must not be board[BOARD_LENGTH-1][] or board[][BOARD_WIDTH-1]
+						if (length<BOARD_LENGTH-1 && width<BOARD_WIDTH-1 && board[length+1][width+1]==0)
 							moves.append((char)(width+'A')).append(length+1).append((char)(width+1+'A')).append(length+2+" ");
-						// If there is an enemy piece to the left with no piece behind it, Mini-Ninja can move there to attack
-						if (length<BOARD_LENGTH-1 && width>0 && board[length][width-1]<0 && board[length+1][width+1]==0)
+						// If there is no piece diagonally forward to the left (board[length+1][width-1]) of Mini-Ninja...
+						// Mini-Ninja must not be board[BOARD_LENGTH-1][] or board[][0]
+						if (length<BOARD_LENGTH-1 && width>0 && board[length+1][width-1]==0)
+							moves.append((char)(width+'A')).append(length+1).append((char)(width-1+'A')).append(length+2+" ");
+						// If there is an enemy piece to the right (board[length][width+1]) with no piece behind it (board[length-1][width+1]), 
+						// Mini-Ninja can move there to attack
+						// Mini-Ninja must not be board[0][] or board[][BOARD_WIDTH-1]
+						if (length>0 && width<BOARD_WIDTH-1 && board[length][width+1]<0 && board[length-1][width+1]==0)
+							moves.append((char)(width+'A')).append(length+1).append((char)(width+1+'A')).append(length+" ");
+						// If there is an enemy piece to the left (board[length][width-1]) with no piece behind (board[length-1][width-1]) it, 
+						// Mini-Ninja can move there to attack
+						// Mini-Ninja must not be board[0][] or board[][0]
+						if (length>0 && width>0 && board[length][width-1]<0 && board[length+1][width+1]==0)
 							moves.append((char)(width+'A')).append(length+1).append((char)(width-1+'A')).append(length+2+" ");
 					}
 				}
